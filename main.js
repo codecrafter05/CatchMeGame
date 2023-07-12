e931562705d64fc383e0c36a825b2c3967b94315
mq1977 <qarooni197@gmail.com>
2023-07-12 11:44:18 +0300

    f2

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const dad = document.querySelector('.mom');
 for (let i = 0; i < 36; i++) {
     let div = document.createElement('div');
     div.classList.add(`box`);
-    div.setAttribute('id',`m${i}`)
+    div.setAttribute('id', `m${i}`)
     dad.appendChild(div);
 }
 
@@ -26,37 +26,44 @@ const boxes = document.querySelectorAll('.box');
 
 
 //image ixtenshen
- const images = [
-'images/monky.png'
+const images = [
+    'images/monky.png'
 ];
 
 
 //location image 
 let currentImageIndex = 15;
 
+// stop the start game button///////
+// console.log(startGame)
 
+
+
+
+// Set the start Game ////
+//  startGame = "start";
 
 
 // Create Global Variables
 
-    let monkeyIntervalId;
+let monkeyIntervalId;
 
-    // Number of times the monkey has been caught
-    let monkeyCaughtCount = 0;
+// Number of times the monkey has been caught
+let monkeyCaughtCount = 0;
 
-    // Set the time delay (monkey show time)
-    let monkeyShowTime = 1000;
+// Set the time delay (monkey show time)
+let monkeyShowTime = 1000;
 
-    // Monkey is in box (default to 0)
-    let monkeyInBox = 0;
+// Monkey is in box (default to 0)
+let monkeyInBox = 0;
 
-    // Set level (starts at 1)
-    let level = 1;
+// Set level (starts at 1)
+let level = 1;
 
-    // Set rounds start at 0
-    let rounds = 0;
+// Set rounds start at 0
+let rounds = 0;
 
-// function to randomly show for the monky 
+
 
 // add one to rounds
 function showMonkey(e) {
@@ -69,11 +76,13 @@ function showMonkey(e) {
     for (let i = 0; i < 36; i++) {
         let div = document.createElement('div');
         div.classList.add(`box`);
-        div.setAttribute('id',`m${i}`)
+        div.setAttribute('id', `m${i}`)
         dad.appendChild(div);
     }
 
     rounds++;
+
+    // Start the status change process by setting the in progress variable//////
     // TODO Move this to somewhere else
     // e.target.setAttribute('disabled', true);
     // Loop for the number of levels
@@ -112,9 +121,9 @@ function showMonkey(e) {
 
     // Add 1 the monkey is in box variable
     setTimeout(checkForMonkey, monkeyShowTime);
- }
+}
 
-    
+
 
 // Set a timer for checking for the monky number of seconds defined by monkey time (call checkForMonkey)
 function catchMonkey(event) {
@@ -124,75 +133,94 @@ function catchMonkey(event) {
     event.target.remove();
     const box = document.querySelector(`#m${currentImageIndex}`);
     box.classList.add('box');
-  
+
     // Remove the class centering to the div
     box.classList.remove('centering');
-  }
 }
 
+// function gameOver(){
+
+//  // Release the progress variable by setting it false////////
+//  startGameChangeInProgress = false;
+// }
+
+
+
+
 // This function looks to see if there is a monkey in the box. IF there is one, the game ends
-function checkForMonkey(){
+function checkForMonkey() {
 
     // Is monkey in the box variable is > 0******
     // he loss if the timer finsh befor he catchit 
 
 
-        // Game ends (stub - fill in with what happens if the game end)
+    // Game ends (stub - fill in with what happens if the game end)
 
     // else
 
-        // Set the level
+    // Set the level
 
-            // level = floor (rounds + 1 / 4) + 1
-            // one level for every 4 rounds
+    // level = floor (rounds + 1 / 4) + 1
+    // one level for every 4 rounds
 
-        // if the level > 10
+    // if the level > 10
 
-            // Set win state of the game
+    // Set win state of the game
 
-            // Return
-
-
-        // if monkey time > 1 && monkey count >= 8
-
-            // reset the monkey count
-
-            // Remove 1/2 second from the monkey show time
-
-        // call the function to show another monkey
+    // Return
 
 
+    // if monkey time > 1 && monkey count >= 8
 
-}    
+    // reset the monkey count
+
+    // Remove 1/2 second from the monkey show time
+
+    // call the function to show another monkey
+
+
+
+}
 
 
 // catch monkey function
 
-    // Add 1 to monkey catch count
+// Add 1 to monkey catch count
 
-    // remove 1 from monkey in the box 
+// remove 1 from monkey in the box 
 
-    // remove the image from the click element
+// remove the image from the click element
 
-    // Add the class 'box' to the div
+// Add the class 'box' to the div
 
-    // Remove the class centering to the div
+// Remove the class centering to the div
 
 
 
 // show the tow monkys at the same time after 4 times he catch the monky 
 
 
-function startGame(){
-    // Start showing monkey
+// enable and disable start button
+function enableStartButton(enable) {
+    if (enable) {
+        const startGameButton = document.getElementById('start');
+        startGameButton.addEventListener('click', startGame);
+    } else {
+        const startGameButton = document.getElementById('start');
+        startGameButton.removeEventListener("click", startGame);
+    }
+}
+
+function startGame() {
     monkeyIntervalId = setInterval(showMonkey, 2000);
+    // Disable 
+    enableStartButton(false);
     // clearInterval(monkeyIntervalId);
 }
 
-function setup(){
+function setup() {
     // Attach click event listener
-    const startGameButton = document.getElementById('start');
-    startGameButton.addEventListener('click',startGame);
+    enableStartButton(true);
     // Once click start game
 }
 
@@ -200,7 +228,7 @@ function setup(){
 document.addEventListener('DOMContentLoaded', (event) => {
     //the event occurred
     setup();
-  })
+})
 
 
 
@@ -223,31 +251,31 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
 
 
-  // Set the date we're counting down to
+// Set the date we're counting down to
 var countDownDate = new Date("jul 14, 2023 11:37:25").getTime();
 
 // Update the count down every 1 second
-var x = setInterval(function() {
+var x = setInterval(function () {
 
-  // Get today's date and time
-  var now = new Date().getTime();
-    
-  // Find the distance between now and the count down date
-  var distance = countDownDate - now;
-    
-  // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
-  // Output the result in an element with id="demo"
-  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
-    
-  // If the count down is over, write some text 
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("demo").innerHTML = "EXPIRED";
-  }
+    // Get today's date and time
+    var now = new Date().getTime();
+
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Output the result in an element with id="demo"
+    document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+        + minutes + "m " + seconds + "s ";
+
+    // If the count down is over, write some text 
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("demo").innerHTML = "EXPIRED";
+    }
 }, 1000);
